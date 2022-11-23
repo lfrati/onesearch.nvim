6e032679bd338253d0266e7bdfba681a6b33ded9
lfrati <fratilapo@gmail.com>
2022-11-23 11:20:12 -0500

    Tweak badge

--- loc.sh
+++ loc.sh
@@ -5,6 +5,6 @@ if [[ $LINES -gt $THRESHOLD ]]
 then
     BADGE_COLOR="red"
 else
-    BADGE_COLOR="green"
+    BADGE_COLOR="brightgreen"
 fi
 printf '{\n  "schemaVersion": 1,\n  "label": "LOC",\n  "message": "%s",\n  "color": "%s"\n}' "$LINES" "$BADGE_COLOR"
